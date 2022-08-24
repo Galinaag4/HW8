cd91bc3129a3c4bd027d3dd4ac398cce81db853c
Galinaag4 <Galinaag@yandex.ru>
2022-08-24 13:40:19 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+     // Задача 1
+    static void visYear(int x) {
+        if (x % 4 == 0 && x % 100 != 0 || x % 400 == 0) {
+            System.out.println(x + "Год считать високосным");
+        } else {
+            System.out.println(x + "Год не считается високосным");
+        }
     }
-}
+
+    public static void main(String[] args){
+        int c = 2021;
+        visYear(c);
+        int z = 2000;
+        int a = 0;
+        parametrs(a,z);
+    }
+    // Задача 2
+    static void parametrs(int oS,int year){
+        if (oS == 0 && year <= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");}
+        if (oS == 1 && year <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Аndroid по ссылке");}}
+    // Задача 3
+
+
+    }
+
+
+
