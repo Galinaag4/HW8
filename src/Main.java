123d7c9cbe94ca413d8de308b16cd70f7a1bb830
Galinaag4 <Galinaag@yandex.ru>
2022-08-26 18:31:58 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,25 @@
 public class Main {
      // Задача 1
-    static void visYear(int x) {
-        if (x % 4 == 0 && x % 100 != 0 || x % 400 == 0) {
-            System.out.println(x + "Год считать високосным");
+    private static void visYear (char year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + "Год считать високосным");
         } else {
-            System.out.println(x + "Год не считается високосным");
+            System.out.println(year + "Год не считается високосным");
         }
     }
 
-    public static void main(String[] args){
-        int c = 2021;
+     static void main(String[] args){
+        char c = 2021;
         visYear(c);
         int z = 2000;
         int a = 0;
         parametrs(a,z);
+        int v= 95;
+        int n = 1;
+        delivery(v,n);
     }
     // Задача 2
-    static void parametrs(int oS,int year){
+    private static void parametrs(int oS,int year){
         if (oS == 0 && year <= 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else {
@@ -26,9 +29,21 @@ public class Main {
         } else {
             System.out.println("Установите версию приложения для Аndroid по ссылке");}}
     // Задача 3.
+    private static void delivery(int deliveryDistance, int deliveryDays){
 
+        if (deliveryDistance > 20) {
+            deliveryDays++;
+        }
+        if (deliveryDistance > 60) {
+            deliveryDays++;
+        }
+        System.out.println("потребуется дней" +  deliveryDays);
+
+        }
 
     }
+
+
 
 
 
